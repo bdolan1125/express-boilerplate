1bfa39bceea54ea987df822c07059dfa2ec7eaad
Benjamin Dolan <benjamin@thinkful.com>
2020-02-20 10:22:57 -0700

    finished boilerplate

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
-'use strict';
-
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
+const { NODE_ENV } = require('./congfig');
 
 const app = express();
 
-const morganOption = process.env.NODE_ENV === 'production'
+const morganOption = NODE_ENV === 'production'
   ? 'tiny'
   : 'common';
 
@@ -22,7 +21,7 @@ app.get('/', (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response
-  if (process.env.NODE_ENV === 'production') {
+  if (NODE_ENV === 'production') {
     response = { error: {message: 'server error'}}
   } else {
     console.error(error)
